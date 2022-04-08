233099369219d42b9fedb40bd84e190630bd8cbb
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-08 13:11:29 +0300

    functia schimbaContinut

--- continut/js/script.js
+++ continut/js/script.js
@@ -1,3 +1,15 @@
+function schimbaContinut(resursa){
+    resursa = resursa + '.html';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange=function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("continut").innerHTML = this.responseText;
+        }
+      };
+    xhttp.open("GET", resursa, true);
+    xhttp.send();
+}
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
