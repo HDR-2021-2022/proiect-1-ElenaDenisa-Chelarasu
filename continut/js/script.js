7740507b43256e35414385cf5a67c9c1a5d7b631
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-08 13:21:20 +0300

    implementare AJAX DONE

--- continut/js/script.js
+++ continut/js/script.js
@@ -1,48 +1,44 @@
-function schimbaContinut(resursa){
+function schimbaContinut(resursa) {
     resursa = resursa + '.html';
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange=function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("continut").innerHTML = this.responseText;
+            document.getElementById("continut").innerHTML = this.responseText;
         }
-      };
+    };
     xhttp.open("GET", resursa, true);
     xhttp.send();
 }
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-
-function sectiunea1(){
-    const date = new Date();
-
-    //transform din data in string
-    const sDate = "Data: " + date.toDateString() + ";";
-    const sTime = "Timpul: " + date.toTimeString() + ";";
-    const sURL = "Adresa URL a paginii: " + window.location.href.toString() + ";";
-    locatie = "Locatia: ";
-    if(navigator.geolocation)
-    {
+function section1() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-        locatie += "lat:" + position.coords.latitude;
-        locatie += ", long:" + position.coords.longitude + ";";
-    }
-    else
-    {
-        locatie += "Nu pot prelua locatia";
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
     }
 
-    var sec1 = {
-        data: sDate,
-        timp: sTime,
-        adrURL: sURL,
-        locatia: locatie
-    }
-    for(int i=0; i<length(sec1); i++)
-    {
-        ;
+    function sectiunea1() {
+        const date = new Date();
+
+        //transform din data in string
+        const sDate = "Data: " + date.toDateString() + ";";
+        const sTime = "Timpul: " + date.toTimeString() + ";";
+        const sURL = "Adresa URL a paginii: " + window.location.href.toString() + ";";
+        locatie = "Locatia: ";
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+            locatie += "lat:" + position.coords.latitude;
+            locatie += ", long:" + position.coords.longitude + ";";
+        }
+        else {
+            locatie += "Nu pot prelua locatia";
+        }
+
+        var sec1 = {
+            data: sDate,
+            timp: sTime,
+            adrURL: sURL,
+            locatia: locatie
+        }
     }
 }
