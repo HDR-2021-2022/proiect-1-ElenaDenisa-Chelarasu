d524a02a6d11b22fbcd6a15bc64dee40d117e970
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-08 13:39:55 +0300

    incarcaLineage (incomplet)

--- continut/js/felidae.js
+++ continut/js/felidae.js
@@ -1,3 +1,20 @@
 function incarcaLineage(){
-    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var xmlDoc = xml.responseXML;
+            var table="<tr><th>Title</th><th>Artist</th></tr>";
+            var x = xmlDoc.getElementsByTagName("CD");
+            for (var i = 0; i <x.length; i++) {
+                table += "<tr><td>" +
+                x[i].getElementsByTagName("lineage")[0].childNodes[0].nodeValue +
+                "</td><td>" +
+                x[i].getElementsByTagName("family")[0].childNodes[0].nodeValue +
+                "</td></tr>";
+            }
+            document.getElementById("continut").innerHTML = table;
+        }
+    };
+    xhttp.open("GET", "felidae.xml", true);
+    xhttp.send();
 }
