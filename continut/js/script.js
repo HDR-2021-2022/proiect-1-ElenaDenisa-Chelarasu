584954f44971188f0b310ff0ac7c60a62fc180b5
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-08 13:34:38 +0300

    creare enviroment pentru pagina felidae.html

--- continut/js/script.js
+++ continut/js/script.js
@@ -1,9 +1,24 @@
-function schimbaContinut(resursa) {
+function schimbaContinut(resursa, jsFisier, jsFunctie) {
     resursa = resursa + '.html';
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("continut").innerHTML = this.responseText;
+            if (jsFisier) {
+                var elementScript = document.createElement('script');
+                elementScript.onload = function () {
+                    console.log("hello");
+                    if (jsFunctie) {
+                        window[jsFunctie]();
+                    }
+                };
+                elementScript.src = jsFisier;
+                document.head.appendChild(elementScript);
+            } else {
+                if (jsFunctie) {
+                    window[jsFunctie]();
+                }
+            }
         }
     };
     xhttp.open("GET", resursa, true);
