4e5efb82cdb57e0c42672bf6b746f48fc1f87e7f
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-13 21:40:25 +0300

    lineage 3

--- continut/js/felidae.js
+++ continut/js/felidae.js
@@ -5,14 +5,15 @@ function incarcaLineage(){
         if (this.readyState == 4 && this.status == 200)
         {
             var xmlDoc = xml.responseXML;
-            var table="<tr><th>Lineage</th><th>Artist</th></tr>";
-            var x = xmlDoc.getElementsByTagName("CD");
+            var table="<table><tr><th>Lineage</th><th>Family</th></tr>";
+            var x = xmlDoc.getElementsByTagName("felidae");
             for (var i = 0; i < x.length; i++)
             {
                 table += "<tr><td>" +
                 x[i].getElementsByTagName("lineage")[0].childNodes[0].nodeValue + "</td><td>" +
                 x[i].getElementsByTagName("family")[0].childNodes[0].nodeValue +"</td></tr>";
             }
+            table +="</table>"
             document.getElementById("continut").innerHTML = table;
         }
     };
