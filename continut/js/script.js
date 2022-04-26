ad9e066fa74af90280bc229e38be9c541fb2957b
Elena-Denisa Chelarasu <elena-denisa.chelarasu@student.tuiasi.ro>
2022-04-26 20:22:39 +0300

    update videoclipuri

--- continut/js/script.js
+++ continut/js/script.js
@@ -25,35 +25,29 @@ function schimbaContinut(resursa, jsFisier, jsFunctie) {
     xhttp.send();
 }
 
-function section1() {
+function f() {
+    //sectiunea 1
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 
-    function sectiunea1() {
-        const date = new Date();
-
-        //transform din data in string
-        const sDate = "Data: " + date.toDateString() + ";";
-        const sTime = "Timpul: " + date.toTimeString() + ";";
-        const sURL = "Adresa URL a paginii: " + window.location.href.toString() + ";";
-        locatie = "Locatia: ";
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-            locatie += "lat:" + position.coords.latitude;
-            locatie += ", long:" + position.coords.longitude + ";";
-        }
-        else {
-            locatie += "Nu pot prelua locatia";
-        }
-
-        var sec1 = {
-            data: sDate,
-            timp: sTime,
-            adrURL: sURL,
-            locatia: locatie
-        }
+    const date = new Date();
+    //transform din data in string
+    const sDate = "Data: " + date.toDateString() + ";";
+    const sTime = "Timpul: " + date.toTimeString() + ";";
+    const sURL = "Adresa URL a paginii: " + window.location.href.toString() + ";";
+    locatie = "Locatia: ";
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        locatie += "lat:" + position.coords.latitude;
+        locatie += ", long:" + position.coords.longitude + ";";
     }
+    else {
+        locatie += "Nu pot prelua locatia";
+    }
+    
+    //afisare
+    const articol = document.getElementById("sectiunea1");
 }
